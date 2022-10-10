06324269608637877fe987dd1d82c0750efacab5
Utku <Utku@192.168.1.35>
2022-10-10 16:51:48 +0300

    odev-3

--- Odev1/src/Main.java
+++ Odev1/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 public static void main(String[] args) {
-	System.err.println();
+	Scanner k=new Scanner(System.in);
+	String ism=k.next();
+	Map<Character, Integer> isim=new TreeMap<>();
+	for (int i = 0; i <ism.length() ;i++) {
+	     isim.put(ism.charAt(i), 0);
+	}
+	for (int i = 0; i <ism.length() ;i++) {
+	     int sayac=isim.get(ism.charAt(i));
+	     isim.put(ism.charAt(i), sayac+1);
+	}
+	
+	System.out.println(isim);
+
 }
 }
